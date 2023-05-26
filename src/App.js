26a3cdea7f19fd94b92e082a8926a3d322bdf92f
yagnadurgesh.k <yagnadurgesh.k@beehyv.com>
2023-05-27 03:26:53 +0530

    chages

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import NoPage from './Components/CommonComponents/NoPage';
 
 
+
+
 export const log = React.createContext();
 export const Categoriesdata = React.createContext();
 
