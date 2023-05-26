2411301eafdf5b458be94f8311b6c63dba5eb30e
yagnadurgesh.k <yagnadurgesh.k@beehyv.com>
2023-05-27 03:57:59 +0530

    new changes for testing revert

--- src/App.js
+++ src/App.js
@@ -16,11 +16,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import NoPage from './Components/CommonComponents/NoPage';
 
 
-
-
-
-
-
 export const log = React.createContext();
 export const Categoriesdata = React.createContext();
 
